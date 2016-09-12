13c0b768dde802b4e4cf8e56583868588d4b282d
Inaba Kazuhiro <inaba.k94@gmail.com>
2016-09-13 02:29:35 +0800

    Add a polymorphic isMutating method

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -40,6 +40,11 @@ public abstract class Command {
      * Executes the command and returns the result.
      */
     public abstract CommandResult execute();
+    
+    /**
+     * Checks if command type mutates the data.
+     */
+    public abstract boolean isMutating();
 
     /**
      * Supplies the data the command will operate on.
